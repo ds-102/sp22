307cf7a5564fa3e1c3ef2bf600abeacc2ac22411
bruns-smith <bruns-smith@berkeley.edu>
2022-03-06 16:52:14 -0800

    q2bi test update

--- labs/lab06/tests/q2bi.py
+++ labs/lab06/tests/q2bi.py
@@ -1,7 +1,8 @@
 test = {   'name': 'q2bi',
     'points': 1,
-    'suites': [   {   'cases': [   {   'code': ">>> checks = [get_hash(worst_forest_predicted_cars_df.index.values[0]) == '6ea9ab1baa0efb9e19094440c317e21b',\n"
-                                               "...           get_hash(worst_forest_predicted_cars_df.index.values[1]) == '17e62166fc8586dfa4d1bc0e1742c08b']\n"
+    'suites': [   {   'cases': [   {   'code': ">>> worst = ['6ea9ab1baa0efb9e19094440c317e21b' , '17e62166fc8586dfa4d1bc0e1742c08b']\n"
+                                               '>>> checks = [get_hash(worst_forest_predicted_cars_df.index.values[0]) in worst,\n'
+                                               '...           get_hash(worst_forest_predicted_cars_df.index.values[1]) in worst ]\n'
                                                '>>> np.all(checks)\n'
                                                'True',
                                        'hidden': False,
